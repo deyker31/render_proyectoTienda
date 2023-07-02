14f24045d879394c2bbadd0f6260e8586d2ebbb1
Deyker Rodriguez <deikerperez10@gmail.com>
2023-07-02 18:21:12 -0400

    NOSE

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+/*
 const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 
+
 server.listen(process.env.PORT || 3001 ,()=>{
     console.log('El servidor esta corriendo');
-})
+}) */
+
+const app1 = require('./app');
+const app2 = require('./models/gorras');
+
+const express = require('express');
+const app = express();
+
+app.use('/app1', app1);
+app.use('/app2', app2);
+
+const server = app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
