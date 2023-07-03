a7a53c4913a9e3e70defe423ab407be965d6a996
Deyker Rodriguez <deikerperez10@gmail.com>
2023-07-03 00:29:00 -0400

    Ahora si

--- prueba.js
+++ prueba.js
@@ -1,10 +1,10 @@
-const { Gorra } = require('./app'); 
+import {Gorra} from './app.js' 
 
+console.log("Veamos");
+console.log(Gorra);
+/*
 Gorra.find({}).then(products => {
   // products contiene todos los documentos de productos 
   // Muestra los productos en tu página HTML
-  mostrarHTML(products);
-})
-function mostrarHTML(products){
-    document.querySelector('body').appendChild(products);
-}
+  
+})*/
