d0e2bc74e7360ac4c8c72fd32da530ea964367cb
Deyker Rodriguez <deikerperez10@gmail.com>
2023-07-07 20:25:16 -0400

    Version 1.1 (Prueba 2)

--- admin_panel/js/app.js
+++ admin_panel/js/app.js
@@ -17,18 +17,18 @@ async function mostrarProductos() {
         
         fila.innerHTML += `
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-            <p class= "text-sm leading-5 font-medium text-gray-700">${nombre}</p>
+            <p class= "text-sm leading-5 font-medium text-gray-700 dark:text-gray-200">${nombre}</p>
         </td>
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-            <p class= "text-sm leading-5 font-medium text-gray-700">${precio}</p>
+            <p class= "text-sm leading-5 font-medium text-gray-700 dark:text-gray-200">${precio}</p>
         </td>
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-            <p class= "text-sm leading-5 font-medium text-gray-700">${marca} - ${tipo}</p>
+            <p class= "text-sm leading-5 font-medium text-gray-700 dark:text-gray-200">${marca} - ${tipo}</p>
         </td>
 
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <a href = "editar-producto.html?id=${_id}" id="${_id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-            <a href="#" data-producto="${_id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+        <a href = "editar-producto.html?id=${_id}" id="${_id}" class="text-teal-600 dark:text-teal-500 hover:text-teal-900 dark:hover:text-teal-600 mr-5">Editar</a>
+            <a href="#" data-producto="${_id}" class="text-red-600 dark:text-red-500 hover:text-red-900 dark:hover:text-red-600 eliminar">Eliminar</a>
         </td>
     `
         listado.appendChild(fila);
